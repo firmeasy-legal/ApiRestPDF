bf8fec9d817f4fa3a6c3380f0e8847d594646209
alexanderdiego2001@outlook.com <[tu@email.com](mailto:tu@email.com)>
2023-11-27 22:18:50 -0500

    asdasd

--- src/modules/editor/infrastructure/persistence/editPdf.ts
+++ src/modules/editor/infrastructure/persistence/editPdf.ts
@@ -23,6 +23,14 @@ type SignatureParams = {
 	file_token: string;
 	time_zone: string;
 	lastUpdate: string;
+	signer_name: string;
+	signer_email: string;
+	signer_ID: string;
+	signer_phone: string;
+	signer_device: string;
+	signer_IP: string;	
+	signer_date: string;
+	qr_link: string;
 };
 
 export class PDFEditor {
@@ -175,7 +183,7 @@ export class PDFEditor {
 				// const page = pdfReader.parsePage(0)
 				// const pageWidth = page.getMediaBox()[2]
 				// const pageHeight = page.getMediaBox()[3]
-				
+
 				const pageWidth = 595
 				const pageHeight = 842
 				console.log("pageWidth:", pageWidth)
@@ -243,23 +251,45 @@ export class PDFEditor {
 							colorspace: "gray",
 							color: 0x31293F,
 							type: "fill",
-							
+
 						})
 
 				// File & QRCode
 
 				pdfWriter
 					.startPageContentContext(nuevoPage)
-					.writeText(signature_params.original_filename,
-						25,
-						pageHeight - (15 * 4) - 45,
+					.writeText(signature_params.original_filename.slice(0, 65),
+						25 + 10,
+						pageHeight - (15 * 4) - 65,
 						{
 							font: pdfWriter.getFontForFile(
-								process.cwd() + "/assets/fonts/NotoSans-Medium.ttf"
+								process.cwd() + "/assets/fonts/NotoSans-Bold.ttf"
 							),
-							size: 15,
+							size: 11,
 							colorspace: "gray",
 							color: 0x000000,
+						})
+					.writeText("Número de Documento ",
+						25 + 10,
+						pageHeight - (15 * 4) - 80,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							colorspace: "gray",
+							color: 0x505050,
+						})
+					.writeText(token,
+						25 + 10 + 95,
+						pageHeight - (15 * 4) - 80,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							colorspace: "gray",
+							color: 0x505050,
 						})
 					.drawImage(
 						pageWidth - 25 - 85,
@@ -274,6 +304,193 @@ export class PDFEditor {
 								fit: "always",
 							}
 						})
+					.drawRectangle(
+						25,
+						pageHeight - (15 * 4) - 120,
+						pageWidth - 25 - 25,
+						1,
+						{
+							colorspace: "gray",
+							color: 0x31293F,
+							type: "fill",
+
+						})
+
+				/* Detalle del firmante */
+				pdfWriter
+					.startPageContentContext(nuevoPage)
+					.writeText("Detalle de Firma",
+						25,
+						pageHeight - (15 * 4) - 145,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Bold.ttf"
+							),
+							size: 12,
+							colorspace: "gray",
+							color: 0x000000,
+						})
+					.drawImage(
+						25,
+						pageHeight - (15 * 4) - 190,
+						"assets/images/png/viñeta.png",
+						{
+							transformation:
+							{
+								width: 25,
+								height: 25,
+								proportional: true,
+								fit: "always",
+							}
+						})
+					.writeText(signature_params.signer_name,
+						25 + 25 + 10,
+						pageHeight - (15 * 4) - 175,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Medium.ttf"
+							),
+							size: 10,
+							colorspace: "gray",
+							color: 0x262626,
+						})
+					.writeText("Firmante",
+						25 + 25 + 10,
+						pageHeight - (15 * 4) - 185,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							// colorspace: "gray",
+							color: 0x0b9f7f,
+						})
+					.writeText("Dispositivo: " + signature_params.signer_device,
+						25 + 25 + 10,
+						pageHeight - (15 * 4) - 220,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							// colorspace: "gray",
+							color: 0x262626,
+						})
+					.writeText("IP: " + signature_params.signer_IP,
+						25 + 25 + 10,
+						pageHeight - (15 * 4) - 232,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							colorspace: "gray",
+							color: 0x262626,
+						})
+					.writeText("Fecha y Hora: " + signature_params.signer_date,
+						25 + 25 + 10,
+						pageHeight - (15 * 4) - 244,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							colorspace: "gray",
+							color: 0x262626,
+						})
+					.writeText("Correo: " + signature_params.signer_email,
+						25 + 25 + 10,
+						pageHeight - (15 * 4) - 256,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							colorspace: "gray",
+							color: 0x262626,
+						})
+					.writeText("Celular: " + signature_params.signer_phone,
+						25 + 25 + 10,
+						pageHeight - (15 * 4) - 268,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							colorspace: "gray",
+							color: 0x262626,
+						})
+					.writeText("FirmEasy ID: " + signature_params.signer_ID,
+						25 + 25 + 10,
+						pageHeight - (15 * 4) - 280,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 8,
+							colorspace: "gray",
+							color: 0x262626,
+						})
+					.drawRectangle(
+						pageWidth - 25 - 195,
+						pageHeight - (15 * 4) - 235,
+						140,
+						70,
+						{
+							color: 0x0e035c,
+						})
+					.drawImage(
+						pageWidth - 25 - 195,
+						pageHeight - (15 * 4) - 235 + 10,
+						signature_params.path_signature,
+						{
+							transformation:
+							{
+								width: 140,
+								height: 140 / 2,
+								proportional: true,
+								fit: "always",
+							}
+						})
+					.writeText("Firma de: " + signature_params.signer_name,
+						pageWidth - 25 - 195,
+						pageHeight - (15 * 4) - 235 + 10 + 140 / 2 + 10,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Regular.ttf"
+							),
+							size: 9,
+							colorspace: "gray",
+							color: 0xffffff,
+						})
+					.drawRectangle(
+						25,
+						pageHeight - (15 * 4) - 300,
+						pageWidth - 25 - 25,
+						1,
+						{
+							colorspace: "gray",
+							color: 0x31293F,
+							type: "fill",
+
+						})
+						
+				const height = pageHeight - (15 * 4) - 315
+
+				/*Add Link QR*/
+				pdfWriter
+					.startPageContentContext(nuevoPage)
+					.writeText("Integridad Documental: "+signature_params.qr_link,
+						25,
+						height,
+						{
+							font: pdfWriter.getFontForFile(
+								process.cwd() + "/assets/fonts/NotoSans-Medium.ttf"
+							),
+							size: 8,
+							// colorspace: "gray",
+							color: 0x333333,
+						})	
 
 				pdfWriter
 					.startPageContentContext(nuevoPage)
