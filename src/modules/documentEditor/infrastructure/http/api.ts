952e1a43850601f680c7d789a6429f8ef54946f7
diego_carlos <alexaderdiego2001@outlook.com>
2024-04-14 19:27:54 -0500

    add last change

--- src/modules/documentEditor/infrastructure/http/api.ts
+++ src/modules/documentEditor/infrastructure/http/api.ts
@@ -198,4 +198,106 @@ apiRouter.post("/addElectronicSignatoryWithSelfie", async (req: Request, res: Re
 	}
 })
 
+apiRouter.post("/addElectronicSignatoryWithDocIdentity", async (req: Request, res: Response) => {
+
+	const {
+		origin_filename,
+		file_path,
+		signature_params
+	} = req.body
+
+	try {
+
+		if (signature_params.doc_identidad) {
+			const normalized_imagen_front_document = signature_params.imagen_front_document.startsWith("/") ? signature_params.imagen_front_document.slice(1) : signature_params.imagen_front_document
+			const normalized_imagen_behind_document = signature_params.imagen_behind_document.startsWith("/") ? signature_params.imagen_behind_document.slice(1) : signature_params.imagen_behind_document
+
+			signature_params.path_imagen_front_document = await s3Repository.getTempPathFromURI_JPG(`public/${normalized_imagen_front_document}`)
+			signature_params.path_imagen_behind_document = await s3Repository.getTempPathFromURI_JPG(`public/${normalized_imagen_behind_document}`)
+		}
+
+		const normalizedFilePath = file_path.startsWith("/") ? file_path.slice(1) : file_path
+
+		const normalizedFilename = origin_filename.startsWith("/") ? origin_filename.slice(1) : origin_filename
+
+		const normalizesQRFilename = signature_params.qr_filename.startsWith("/") ? signature_params.qr_filename.slice(1) : signature_params.qr_filename
+
+		const path_file = await s3Repository.getTempPathFromURI_PDF(`public/${normalizedFilename}`)
+
+		if (!path_file) {
+			return res.status(401).json({
+				message: "Hubo un error al obtener el PDF"
+			})
+		}
+
+		signature_params.path_signature = await s3Repository.getTempPathFromURI_PNG(`public/${signature_params.signature_filename}`)
+
+		if (!signature_params.path_signature) {
+			return res.status(401).json({
+				message: "Hubo un error al obtener la firma"
+			})
+		}
+
+		signature_params.qr_filename = await s3Repository.getTempPathFromURI_PNG(`public/${normalizesQRFilename}`)
+
+		if (!signature_params.qr_filename) {
+			return res.status(401).json({
+				message: "Hubo un error al obtener el QR"
+			})
+		}
+
+		const pdf_signed = await documentRepository.addInitialSignature(path_file, signature_params)
+
+		if (!pdf_signed) {
+			return res.status(401).json({
+				message: "Hubo un error al procesar el PDF no firmado"
+			})
+		}
+
+		const pdf_summary_added = await documentRepository.addSummarySignatureWithDocIdentity(pdf_signed, signature_params)
+
+		if (!pdf_summary_added) {
+			return res.status(401).json({
+				message: "Hubo un error al procesar el PDF firmado"
+			})
+		}
+
+		const result = await s3Repository.addFileToS3(pdf_summary_added, normalizedFilePath)
+
+		if (result === undefined) {
+			res.status(500).json({
+				error: "Ocurrió un error al guardar el archivo en S3",
+				message: "La función devolvió undefined, probablemente hubo un problema al guardar el archivo"
+			})
+		} else {
+			const { fileKey, new_filename, file_path } = result
+
+			res.json({
+				completePath: fileKey,
+				new_filename,
+				file_path
+			})
+		}
+
+		filerepository.deleteFile(path_file)
+		filerepository.deleteFile(signature_params.path_signature)
+		filerepository.deleteFile(signature_params.qr_filename)
+		filerepository.deleteFile(pdf_signed)
+		filerepository.deleteFile(pdf_summary_added)
+
+		if (signature_params.doc_identidad) {
+			filerepository.deleteFile(signature_params.path_imagen_front_document)
+			filerepository.deleteFile(signature_params.path_imagen_behind_document)
+		}
+
+	} catch (error) {
+		loggerRepository.error(error)
+		console.error("Error:", error)
+		return res.status(500).json({
+			message: "Error al procesar el PDF",
+			error: error
+		})
+	}
+})
+
 export { apiRouter }
